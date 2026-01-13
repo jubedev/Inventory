9cb5bee0ff416720a30a14e0638e19d132163929
jubedev <hearthismusic808@gmail.com>
2026-01-13 15:39:15 -0500

    fix: eliminar uso de hook useMovimientos y simplificar la barra de búsqueda

--- frontend/src/features/movimientos_inventario/pages/MovimientosListPage.jsx
+++ frontend/src/features/movimientos_inventario/pages/MovimientosListPage.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
-import { useMovimientos } from '../../../hooks/useMovimientos';
+import Icon from "../../../components/ui/Icon";
 
 const MovimientosListPage = () => {
-
-    const [ movimientos, loading, error, fetchMovimientos, deleteMovimientos ] = useMovimientos()
-
   return (
     <div className="p-8 min-h-screen">
       <div className="mb-8">
@@ -14,10 +11,22 @@ const MovimientosListPage = () => {
         <p className="text-gray-600">Gestiona los movimientos de inventario</p>
       </div>
 
-      {/* Barra de acciones */}
-      <div className="bg-white rounded-2xl p-4 mb-6 justify-between flex flex-col md:flex-row items-center shadow-md gap-4">
-        <input type="search" name="" id="" />
-      </div>
+      <div className="bg-white rounded-lg shadow-md p-4 mb-6 flex flex-col md:flex-row gap-4 justify-between items-center">
+          <div className="flex-1 w-full md:w-auto">
+            <input
+              type="text"
+              placeholder="Buscar por activo, serial, marca, modelo o ubicación..."
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <button
+            className="w-full md:w-auto px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all font-medium flex items-center justify-center gap-2"
+          >
+            <span className="text-xl"><Icon name="filter" className="w-6 h-6" /></span>
+            Buscar Por
+          </button>
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-4 mb-6 flex flex-col md:flex-row gap-4 justify-between items-center"></div>
     </div>
   );
 };
